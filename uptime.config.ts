bb113fb41e50cb8fef03bcb26dafd4e60d708995
YUN-LOVE <YUN-LOVE@031312.xyz>
2026-06-08 20:39:31 +0800

    use webhook to send warning message

--- uptime.config.ts
+++ uptime.config.ts
@@ -233,7 +233,7 @@ const workerConfig: WorkerConfig = {
     // More info at Wiki: https://github.com/lyc8503/UptimeFlare/wiki/Setup-notification
     webhook: {
       // [Required] webhook URL (example: Telegram Bot API)
-      url: 'https://push.031312.xyz/api/push/y8Vo5RcCHRAI1wM8',
+      url: 'https://push.031312.xyz/api/push/y8Vo5RcCHRAIiwM8',
       // [Optional] HTTP method, default to 'GET' for payloadType=param, 'POST' otherwise
       method: 'POST',
       // [Optional] headers to be sent
